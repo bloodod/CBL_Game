6ca2bd27665b4b8cf88b979d7a6ad965c5253900
Danailo1 <d.h.stessen@student.tue.nl>
2024-10-17 10:52:51 +0200

    Creating the endpage

---
+++ EndPage.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class EndPage implements ActionListener {
+    JFrame frame;
+    JButton buttonPlayAgain;
+    JButton buttonExit;
+    JLabel highscoreLabel;
+
+
+    EndPage() {
+        frame = new JFrame("End screen");
+        buttonPlayAgain = new JButton("Play again");
+        buttonExit = new JButton("Exit");
+        highscoreLabel = new JLabel("Highscore: ");
+
+        buttonPlayAgain.setBounds(300,150,200,40);
+        buttonPlayAgain.setFocusable(false);
+        buttonPlayAgain.addActionListener(this);
+
+        buttonExit.setBounds(300,300,200,40);
+        buttonExit.setFocusable(false);
+        buttonExit.addActionListener(this);
+
+        highscoreLabel.setBounds(300,50,400,40);
+        highscoreLabel.setFont(new Font(null,Font.PLAIN,25));
+
+        frame.add(buttonPlayAgain);
+        frame.add(buttonExit);
+        frame.add(highscoreLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);      //Set window to the middle of screen
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
